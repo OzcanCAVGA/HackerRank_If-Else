bcf5952a25db6d7e8c3c4c8e2b1bb9019d8b8ef3
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:04:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int n ;
+        Scanner input = new Scanner (System.in);
+
+        n=input.nextInt();
+
+        if ((n>=1) && (n<=100)){
+            if ((n%2 ==0) && (n>=2) && (n<=5)) {
+                System.out.println("Not Weird");
+            }
+            else if ((n%2 == 0) && (n>20)) {
+                System.out.println("Not Weird") ;
+            }
+            else {
+                System.out.println("Weird") ;
+            }
+        }
     }
 }
